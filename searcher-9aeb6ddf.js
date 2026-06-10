445736907951656691ddc3e38560b1ad45d118a0
ebfull <ebfull@users.noreply.github.com>
2026-06-10 18:20:57 +0000

    deploy: 33a69017f8097d118e7f47ebd14305ff6d7f79e7

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-edba9c4c.js',
+                path_to_root + 'searchindex-36081bb6.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
