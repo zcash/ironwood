5d75123dbccd92750dbd6ddc1a0213e3303ef820
mitschabaude <mitschabaude@users.noreply.github.com>
2026-07-28 14:59:54 +0000

    deploy: 591454c22e25cd108a1b3e0726b8daabf006fc51

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bf43ce7e.js',
+                path_to_root + 'searchindex-04480e57.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
