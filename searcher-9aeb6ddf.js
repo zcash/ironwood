902abc07c13a61be5f2876990feb5f501b97417b
TalDerei <TalDerei@users.noreply.github.com>
2026-07-29 17:18:26 +0000

    deploy: 7c69a80139c16ccc96c57175ba58a860f49075b0

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d454e67d.js',
+                path_to_root + 'searchindex-afc3aa59.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
