ada3aafd091646e66b1e6a82b37caa702f04f289
daira <daira@users.noreply.github.com>
2026-08-15 23:00:13 +0000

    deploy: cfb8901f0ccb53bf720c2a6acc87d279bd918b50

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-509bef4a.js',
+                path_to_root + 'searchindex-cdaef831.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
