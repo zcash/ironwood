548f8a8d64e893354555c02cf666104e30dda6f8
mitschabaude <mitschabaude@users.noreply.github.com>
2026-08-27 09:16:26 +0000

    deploy: 1628b4cdd2a18d05a8dacedf9ee27d6a322573e7

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-80f93269.js',
+                path_to_root + 'searchindex-638ed659.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
